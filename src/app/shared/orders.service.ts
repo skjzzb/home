48fe6f5d0898c913d775b04919ba962d49f553d7
danushka96 <danushkaherath96@gmail.com>
2019-08-15 22:04:14 +0530

    fix ui in home

--- src/app/shared/orders.service.ts
+++ src/app/shared/orders.service.ts
@@ -6,6 +6,7 @@ import {map} from 'rxjs/operators'
 import { CustomerRequest } from '../models/CustomerRequest.model';
 import { ToastrService } from 'ngx-toastr';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
@@ -16,7 +17,8 @@ export class OrdersService {
   orders: AngularFireList<CustomerRequest>
   countTempOrders: Observable<OrderData[]>;
   count: Observable<number>;
-  constructor(private firebase: AngularFireDatabase, private toastr: ToastrService, private router: Router) { }
+  constructor(private firebase: AngularFireDatabase, private toastr: ToastrService, private router: Router,
+              private http:HttpClient) { }
 
   getTempOrders(){
     const user = JSON.parse(localStorage.getItem('user'));
@@ -56,6 +58,15 @@ export class OrdersService {
       this.firebase.object('CustomerRequests/'+user.uid+'/'+timestamp+'/order/'+count).set(tempOrder);
       count++
     })
+
+    var queryString = 'https://us-central1-picknwash-56236.cloudfunctions.net/findDriver?location='
+                      +order.pickupLocation+'&customer='+user.uid+'&pieces='+order.totalItems+
+                      '&order='+timestamp
+    console.log(queryString)
+    this.http.get(queryString).subscribe((data)=>{
+      console.log(data)
+    })
+    
     this.firebase.object('tempOrders/'+user.uid).remove()
     this.showSuccess()
     this.router.navigate(['orders']);
